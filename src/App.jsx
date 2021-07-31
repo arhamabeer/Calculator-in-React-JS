20a5801bf517fbe602484b78d88876ef66ceda93
arhamabeer <arhamabeerahmed@gmail.com>
2021-07-31 19:12:15 +0500

    second commit after functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,44 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [txt, setTxt] = useState('');    
+  const [txt, setTxt] = useState("");
+  const [res, setRes] = useState("");
+
+
   var btnFunc = (v) => {
     // console.log(v)
-    let num = txt + v
+    let num = txt + v;
     // console.log(num)
-    setTxt(num)
+    setTxt(num);
+    setRes("");
   };
   var calculate = () => {
-    console.log(eval(txt))
-    setTxt((eval(txt)))
+    let res = eval(txt)
+    setTxt('');
+    console.log(res)
+    setRes(res)
   };
+
+  var check = () => {
+    let num = txt;
+    console.log(num)
+    let arr = [...num];
+    let val = arr[arr.length - 1];
+
+    if (val === "/" || val === "*" || val === "+" || val === "-") {
+      document.getElementById("equal").disabled = true;
+    } else if (document.getElementById("equal") === null) {
+    } else {
+      document.getElementById("equal").disabled = false;
+
+    } 
+  };
+
   var clr = () => {
-    setTxt('')
+    setTxt("");
+    setRes("");
   };
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,26 +47,126 @@ function App() {
       </header>
       <div className="div-main">
         <div className="div-inp">
-          <input className="inp" value={txt} type="text" />
+          <input className="inp" onChange={check()} value={txt} type="text" />
+          <h1 className='h1-res'>{res}</h1>
         </div>
-        <button style={{width: '60%', marginLeft: '100px', height: '35px', fontSize: '25px',backgroundColor: 'skyblue'}} onClick={(e)=>{clr()}}>CLEAR</button>
+        <button
+          className="clr-btn"
+          onClick={(e) => {
+            clr();
+          }}
+        >
+          CLEAR
+        </button>
         <div className="div-btn">
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>7</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>8</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>9</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>/</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>4</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>5</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>6</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>*</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>1</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>2</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>3</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>-</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>0</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>.</button>
-          <button onClick={()=>calculate()}>=</button>
-          <button onClick={(e)=>{btnFunc(e.target.textContent)}}>+</button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            7
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            8
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            9
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            /
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            4
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            5
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            6
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            *
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            3
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            0
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            .
+          </button>
+          <button id="equal" onClick={() => calculate()}>
+            =
+          </button>
+          <button
+            onClick={(e) => {
+              btnFunc(e.target.textContent);
+            }}
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
